ad740b2d5b60cb80342c3d3940867f4c64349f86
nafilah17 <rissanafilah@gmail.com>
2020-07-10 11:02:43 +0700

    lesson 3 model attributes, tinker, data transformers

---
+++ app/Contacts.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contacts extends Model
+{
+    protected $table = 'TblContacts';
+    protected $primaryKey = 'Contacts_ID';
+
+    public function contactName(){
+    	return $this->Contacts_Name;
+    }
+}
